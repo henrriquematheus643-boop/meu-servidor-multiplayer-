f14f2a18a9afd7feda8d437af6cc7cf941bc1523
henrriquematheus643-boop <henrriquematheus643@gmail.com>
2026-05-24 13:26:53 -0300

    server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "servidor-multiplayer",
+  "version": "1.0.0",
+  "description": "Servidor de suporte para Godot",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "ws": "^8.18.0"
+  }
+}
