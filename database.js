58bff6b36dba13d3e26accf39e5d554163bc1b96
henrriquematheus643-boop <henrriquematheus643@gmail.com>
2026-05-27 18:04:32 -0300

    Update database.js

--- database.js
+++ database.js
@@ -2,31 +2,41 @@ const { MongoClient } = require('mongodb');
 
 const uri = "mongodb+srv://redutorp:rp123@cluster0.v8k3m.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
+let db;
 let contas;
 
 async function conectar() {
     try {
         await client.connect();
-        const db = client.db("reduto_rp");
+        db = client.db("reduto_rp");
         contas = db.collection("usuarios_permanentes");
-        console.log("Nuvem conectada!");
-    } catch (e) { console.log("Erro nuvem:", e); }
+        console.log("[Nuvem] Conexão estabelecida com sucesso!");
+    } catch (e) {
+        console.error("[Nuvem] Erro ao conectar:", e);
+    }
 }
 conectar();
 
 async function buscarUsuarioUnico(nome) {
-    if (!contas) return null;
-    return await contas.findOne({ username: nome });
+    try {
+        if (!contas) return null;
+        // Busca ignorando se é maiúscula ou minúscula para evitar bugs
+        return await contas.findOne({ username: { $regex: new RegExp("^" + nome + "$", "i") } });
+    } catch (e) { return null; }
 }
 
 async function salvarUsuarioDireto(dados) {
-    if (!contas) return;
-    await contas.updateOne({ username: dados.username }, { $set: dados }, { upsert: true });
+    try {
+        if (!contas) return;
+        await contas.updateOne({ username: dados.username }, { $set: dados }, { upsert: true });
+    } catch (e) {}
 }
 
 async function salvarPosicaoPlayer(nome, posicao) {
-    if (!contas) return;
-    await contas.updateOne({ username: nome }, { $set: { last_pos: posicao } });
+    try {
+        if (!contas) return;
+        await contas.updateOne({ username: nome }, { $set: { last_pos: posicao } });
+    } catch (e) {}
 }
 
 module.exports = { buscarUsuarioUnico, salvarUsuarioDireto, salvarPosicaoPlayer };
